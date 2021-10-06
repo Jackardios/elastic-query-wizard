8e9f652c5c07ad179e099f29d5ad16ab393f3421
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-10-06 06:39:41 +0300

    fix type

--- src/Handlers/Sorts/CallbackSort.php
+++ src/Handlers/Sorts/CallbackSort.php
@@ -3,18 +3,18 @@
 namespace Jackardios\ElasticQueryWizard\Handlers\Sorts;
 
 use Illuminate\Database\Eloquent\Builder;
-use Jackardios\QueryWizard\Abstracts\Handlers\AbstractQueryHandler;
+use Jackardios\ElasticQueryWizard\Handlers\ElasticQueryHandler;
 
 class CallbackSort extends AbstractElasticSort
 {
     /**
-     * @var callable(AbstractQueryHandler, Builder, string, string):mixed
+     * @var callable(ElasticQueryHandler, Builder, string, string):mixed
      */
     private $callback;
 
     /**
      * @param string $propertyName
-     * @param callable(AbstractQueryHandler, Builder, string, string):mixed $callback
+     * @param callable(ElasticQueryHandler, Builder, string, string):mixed $callback
      * @param string|null $alias
      */
     public function __construct(string $propertyName, callable $callback, ?string $alias = null)
